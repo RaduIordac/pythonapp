4d622ad2a96053d3235ced843852bcd371815c6e
raduiordache <radu_iordache_83@yahoo.com>
2022-07-12 13:56:00 +0300

    2comit

---
+++ mysite/polls/radio.py
@@ -0,0 +1,19 @@
+import vlc
+
+
+url = 'https://coderadio.freecodecamp.org/'
+
+#define VLC instance
+instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
+
+#Define VLC player
+player=instance.media_player_new()
+
+#Define VLC media
+media=instance.media_new(url)
+
+#Set player media
+player.set_media(media)
+
+#Play the media
+play_it = player.play()
